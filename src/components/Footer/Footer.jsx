32eca56b36b1e616b8df4c1565255d468b8f78c8
anouar garbaa <g.anouar@yahoo.com>
2024-06-10 01:03:30 +0100

    add date to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import style from './Footer.module.css'
+import React, { useState } from 'react';
+import style from './Footer.module.css';
 
 export default function Footer() {
- 
+  const [currentDate, setCurrentDate] = useState(new Date());
 
   return (
     <div className="fixed bottom-0 w-full flex justify-center items-center bg-gray-600 text-white py-8 px-4">
-      Copyright &copy; 2024 - ANOUAR
+      Copyright &copy; {new Date().getFullYear()} - ANOUAR
     </div>
-  )
-  
+  );
 }
+
